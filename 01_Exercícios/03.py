7d410385f0bb1866b55d1df31ca4887f5444f454
Marco Bispo <marcobispo97@gmail.com>
2023-11-05 16:38:39 -0300

    exercicios

---
+++ 01_Exercícios/03.py
@@ -0,0 +1,11 @@
+# 3. Desenvolva um programa que represente um animal. O programa deve ter uma classe Animal que tenha os atributos nome, espécie, idade e habitat. O programa deve também ter um método imprimir_dados() que imprima os dados do animal.
+
+# Enredo:
+
+# Imagine que você está desenvolvendo um programa para um zoológico. Você precisa criar um programa que possa representar um animal. O programa deve ter os seguintes atributos:
+
+# Nome: o nome do animal
+# Espécie: a espécie do animal
+# Idade: a idade do animal
+# Habitat: o habitat do animal
+# O programa também deve ter um método imprimir_dados() que imprima os dados do animal.
