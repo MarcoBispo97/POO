6662f2be300a08b8604623605c498031826e1a6d
Marco Bispo <marcobispo97@gmail.com>
2023-11-06 09:16:04 -0300

    organização

---
+++ 01_Exercícios/01.py
@@ -0,0 +1,11 @@
+# 1. Desenvolva um programa que represente um carro. O programa deve ter uma classe Carro que tenha os atributos cor, modelo, ano e placa. O programa deve também ter um método imprimir_dados() que imprima os dados do carro.
+
+# Enredo:
+
+# Imagine que você está desenvolvendo um programa para uma concessionária de carros. Você precisa criar um programa que possa representar um carro. O programa deve ter os seguintes atributos:
+
+# Cor: a cor do carro
+# Modelo: o modelo do carro
+# Ano: o ano de fabricação do carro
+# Placa: a placa do carro
+# O programa também deve ter um método imprimir_dados() que imprima os dados do carro.
---
+++ 01_Exercícios/02.py
@@ -0,0 +1,11 @@
+# 2. Desenvolva um programa que represente uma pessoa. O programa deve ter uma classe Pessoa que tenha os atributos nome, idade, sexo e endereço. O programa deve também ter um método imprimir_dados() que imprima os dados da pessoa.
+
+# Enredo:
+
+# Imagine que você está desenvolvendo um programa para uma empresa. Você precisa criar um programa que possa representar uma pessoa. O programa deve ter os seguintes atributos:
+
+# Nome: o nome da pessoa
+# Idade: a idade da pessoa
+# Sexo: o sexo da pessoa
+# Endereço: o endereço da pessoa
+# O programa também deve ter um método imprimir_dados() que imprima os dados da pessoa.
